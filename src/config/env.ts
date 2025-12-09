8c6ef973cbc70cafc3a9aabe8bbfd025a11ecc6e
johnshift <johnshift.dev@gmail.com>
2025-12-10 00:20:43 +0800

    feat: add alchemy key env

--- src/config/env.ts
+++ src/config/env.ts
@@ -4,10 +4,12 @@ const envSchema = z.object({
   NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID: z
     .string()
     .min(1, 'Rainbowkit Project ID is required'),
+  NEXT_PUBLIC_ALCHEMY_KEY: z.string().min(1, 'Alchemy Key is required'),
 });
 
 const parsed = envSchema.safeParse({
   NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID,
+  NEXT_PUBLIC_ALCHEMY_KEY: process.env.NEXT_PUBLIC_ALCHEMY_KEY,
 });
 
 if (!parsed.success) {
